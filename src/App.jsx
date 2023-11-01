704a603a7db76bc8e7bd89ce763b40b9055fbe5c
tiago.inovatech <tiago.inovatech.inf@gmail.com>
2023-11-01 11:26:20 -0300

    37. JSX & React Components [Core Concept]

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import logo from "./assets/react-core-concepts.png"
+
+function App() {
+  return (
+    <div>
+      <header>
+        <img src={logo} alt="Stylized atom" />
+        <h1>React Essentials</h1>
+        <p>
+          Fundamental React concepts you will need for almost any app you are
+          going to build!
+        </p>
+      </header>
+      <main>
+        <h2>Time to get started!</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
