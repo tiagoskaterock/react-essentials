4c7d1847a8800b536de2543cdd20cd5985e66372
tiago.inovatech <tiago.inovatech.inf@gmail.com>
2023-11-02 11:37:56 -0300

    Usando aleatoriedade na frase de apreesentação

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,17 @@
 import logo from "../assets/react-core-concepts.png"
 
+function stringAleatoria() {  
+  const meuArray = ["Crucial", "Core", "Fundamental"]
+  return meuArray[Math.floor(Math.random() * meuArray.length)];
+}
+
 function Header() {
 	return (
 		<header>
       <img src={logo} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {stringAleatoria()} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
